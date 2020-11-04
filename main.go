767582c1a5bf36f350e5a9450f7fb4e2f57067cc
xiaohy <310363403@qq.com>
2020-11-04 22:00:20 +0800

    重置

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("double debug")
-	fmt.Println("double master")
-	fmt.Println("change now")
-	fmt.Println("double dev")
-	fmt.Println("change dev now")
+	fmt.Println("new")
 }
