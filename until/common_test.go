76fec7a5baf5767eff8a6665a72343e2956f9585
xiaohy <310363403@qq.com>
2020-11-04 21:48:14 +0800

    添加单元测试

---
+++ until/common_test.go
@@ -0,0 +1,26 @@
+package until
+
+import "testing"
+
+func Test_add(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{1,2}, 3},
+		{"test2", args{1,2}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
